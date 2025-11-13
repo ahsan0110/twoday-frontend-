aa759e5e08bca5c4d3e5be2b60743237b719e672
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-13 21:39:43 +0500

    Added different files for Mobile and tablet responsive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '/src/style.css'
-import '/src/responsive.css' 
+import '/src/tablet.css'
+import '/src/mobile.css' 
 
 const app = createApp(App);
 
