6a96ea4c9a6d1951b22f4f3c9bb3fd928ad0daee
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-14 18:15:08 +0500

    completed the lastingImpact component also made changes to navbar and hero section

--- src/components/LastingImpact.vue
+++ src/components/LastingImpact.vue
@@ -14,15 +14,34 @@
     <section class="last-impact-section">
         <div class="blur-impact">
             <img src="ultrasound.png" alt="">
+            <div class="impact-text">
+                <p>Together with Päijät-Häme Welfare Region, we’re using AI to make everyday life
+                    simpler for both residents and professionals — easing workloads, improving access to care, and
+                    shaping a
+                    smarter, more human welfare service.</p>
+                <a class="staff-link read-more">Read More</a>
+            </div>
         </div>
         <div class="blur-impact">
             <img src="blurImage1.png" alt="">
+            <div class="impact-text">
+
+                <p>ISS saves hundreds of hours of working time by modernizing its data platform and
+                    customer reporting.</p>
+                <a class="staff-link read-more">Read More</a>
+            </div>
         </div>
         <div class="blur-impact">
             <video autoplay muted loop playsinline class="blur-video">
                 <source src="water.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+            <div class="impact-text">
+
+                <p>A future-proof platform that brings systems together, simplifies daily work, and
+                    helps our client move faster. Because real progress starts with a solid digital foundation.</p>
+                <a class="staff-link read-more">Read More</a>
+            </div>
         </div>
     </section>
 
