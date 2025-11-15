9d235761e9c946df0f9c334e7f5040b6a2bfff63
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-15 18:00:49 +0500

    completed levarage component of service page

--- src/Pages/ServicePage.vue
+++ src/Pages/ServicePage.vue
@@ -1,12 +1,25 @@
 <script>
 import NavbarComponent from '@/components/NavbarComponent.vue';
 import HeroSection from '@/components/HeroSection.vue';
+import LeveraGing from '@/components/LeveraGing.vue';
+// import LastingImpact from '@/components/LastingImpact.vue'
+// import FrankComponent from '@/components/FrankComponent.vue'
+// import LatestNews from '@/components/LatestNews.vue'
+// import FormComponent from '@/components/FormComponent.vue'
+// import FooterComponent from '@/components/FooterComponent.vue'
 
 export default {
     name: 'ServicePage',
     components: {
         NavbarComponent,
         HeroSection,
+        LeveraGing,
+        // LastingImpact,
+        // FrankComponent,
+        // LatestNews,
+        // FormComponent,
+        // FooterComponent,
+        
     }
 }
 </script>
@@ -14,12 +27,22 @@ export default {
 <template>
     <NavbarComponent />
     <HeroSection 
-    smallText="OUR SERVICES" 
-    largeText="Anyone can talk about the future.<br>We engineer it." 
-    ctaText="Get in Touch"
-    videoSrc=""
-    heroClass="service-hero"
+        smallText="OUR SERVICES" 
+        largeText="Anyone can talk about the future.<br>We engineer it." 
+        ctaText="Get in Touch"
+        videoSrc=""
+        heroClass="service-hero"
     />
+    <LeveraGing 
+        class="service-lev"
+        heading=""
+        subHeading="We apply AI to create impactful solutions in:"
+    />
+    <!-- <LastingImpact />
+    <FrankComponent />
+    <LatestNews />
+    <FormComponent />
+    <FooterComponent /> -->
 
 
 </template>
--- src/components/LeveraGing.vue
+++ src/components/LeveraGing.vue
@@ -1,32 +1,13 @@
 <template>
-    <div class="lev-section">
-        <h1 class="leverage-heading">We help organizations apply and scale AI - grounded in strong data, cloud, and
-            engineering</h1>
-        <h2>Learn how we create impactful solutions leveraging:</h2>
+    <div :class="levClass">
+        <h1 class="leverage-heading">{{ heading }}</h1>
+        <h2>{{ subHeading }}</h2>
         <div class="lev-component">
-            <div class="card">
-                <span class="small-info">01</span>
-                <h1 class="card-label">Data</h1>
-                <p class="card-para">We specialize in harnessing the power of data and artificial intelligence (AI) to drive businesses forward. Our data and AI services are designed to empower organizations with solutions that enhance decision-making and fuel business outcomes.</p>
-                <a href="">Become data driven <img src="right-up.png" width="20px" alt=""></a>
-            </div>
-            <div class="card">
-                <span class="small-info">02</span>
-                <h1 class="card-label">Cloud</h1>
-                <p class="card-para">Our Cloud Platforms and Security experts help enterprises maximize return on their investments in Microsoft Systems Management and Cloud Platform Solutions.</p>
-                <a href="">Explore cloud solutions <img src="right-up.png" width="20px" alt=""></a>
-            </div>
-            <div class="card">
-                <span class="small-info">03</span>
-                <h1 class="card-label">Engineering</h1>
-                <p class="card-para">At Twoday we drive digital transformation through custom software development, leveraging cloud services and application expertise. We utilize the latest technologies, to develop high-quality and scalable software solutions following industry best practices.</p>
-                <a href="">Start building <img src="right-up.png" width="20px" alt=""></a>
-            </div>
-            <div class="card">
-                <span class="small-info">04</span>
-                <h1 class="card-label">Applications</h1>
-                <p class="card-para">Looking to elevate your business operations? Partner with us to implement business applications that not only reduce costs but also increase efficiency. With our solutions for sales, marketing, customer service, and more, we're dedicated to your success.</p>
-                <a href="">Explore Applications <img src="right-up.png" width="20px" alt=""></a>
+            <div class="card" v-for="(item, index) in cards" :key="index">
+                <span class="small-info">{{ item.number }}</span>
+                <h1 class="card-label">{{ item.title }}</h1>
+                <p class="card-para">{{ item.description }}</p>
+                <a :href="item.ctaLink" >{{ item.ctaText }}</a>
             </div>
         </div>
 
@@ -35,5 +16,44 @@
 <script>
 export default {
     name: "LeveraGing",
+    props: {
+        levClass:{ type:String,default:"lev-section"},
+        heading: { type: String, default: "We help organizations apply and scale AI - grounded in strong data, cloud, and engineering" },
+        subHeading: { type: String, default: "Learn how we create impactful solutions leveraging:" },
+        cards: {
+            type: Array,
+            default: () => [
+                {
+                    number: "01",
+                    title: "Data",
+                    description: "We specialize in harnessing the power of data and artificial intelligence (AI) to drive businesses forward. Our data and AI services are designed to empower organizations with solutions that enhance decision-making and fuel business outcomes.",
+                    ctaText: "Become data driven",
+                    ctaLink: "#",
+                },
+                {
+                    number: "02",
+                    title: "Cloud",
+                    description: "Our Cloud Platforms and Security experts help enterprises maximize return on their investments in Microsoft Systems Management and Cloud Platform Solutions.",
+                    ctaText: "Explore cloud solutions",
+                    ctaLink: "#",
+                },
+                {
+                    number: "03",
+                    title: "Engineering",
+                    description: "At Twoday we drive digital transformation through custom software development, leveraging cloud services and application expertise. We utilize the latest technologies, to develop high-quality and scalable software solutions following industry best practices.",
+                    ctaText: "Start building",
+                    ctaLink: "#",
+                },
+                {
+                    number: "04",
+                    title: "Applications",
+                    description: "Looking to elevate your business operations? Partner with us to implement business applications that not only reduce costs but also increase efficiency. With our solutions for sales, marketing, customer service, and more, we're dedicated to your success.",
+                    ctaText: "Explore Applications",
+                    ctaLink: "#",
+                }
+            ]
+        }
+
+    }
 };
 </script>
