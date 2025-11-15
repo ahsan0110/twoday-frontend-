0a4e03f769d8a2d859decd189b762e06682ce2cb
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-15 23:26:10 +0500

    completed frank component of career

--- src/Pages/CareerPage.vue
+++ src/Pages/CareerPage.vue
@@ -60,22 +60,20 @@ export default {
 
     <HeroSection smallText="CAREERS" largeText="Tomorrow is built by the<br>people who engineer it"
         ctaText="Talk to an expert" ctaLink="#form-section-id" videoSrc="Industries-Hero.mp4" heroClass="career-hero" />
-    <LeveraGing class="career-lev" heading="" subHeading="We build a workplace where people grow and belong:"
+    <LeveraGing class="career-lev" 
+        heading="" 
+        subHeading="We build a workplace where people grow and    belong:"
         :cards="careerCard" />
 
-
-
-
     <FrankComponent
-        rightPara="Every day, we apply AI and engineering to solve what matters. Building solutions that improve lives across both public and private sectors."
-        expertTag="Customer Story" expertNameRole="Become Expert<br>with Spar Nord " expertCTA="Read More"
+        expertTag="Meet Our People" expertNameRole="Meet Frank<br>AI & Engineering " expertCTA="Read More"
         quote="To meet increasing demands for greater data compliance, speed, and quality, Danish bank Spar Nord made a strategic decision: to build its own data platform."
-        imageSrc="Spar-Nord-Sign.png" frankTextClass="text-large-service" />
+        imageSrc="uncle.png" frankTextClass="text-large-service" />
 
 
 
     <FormComponent formTextClass="mainText-services" formParaClass="form-para-services"
-        mainText="Unlock the power of data. Turn decisions into growth." formSectionClass="form-section-services"
+        mainText="Say Something BIG..." formSectionClass="form-section-services"
         mainPara="Twoday is the defining force in applied AI and advanced engineering. With 3,000 experts in data, software, and cloud engineering, we cut through complexity, remove uncertainty, and prove the value of technology.
 
         We strengthen private and public organizations across industries like energy and utilities, industrials and manufacturing, health and life sciences, and government and more.
