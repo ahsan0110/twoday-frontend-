1f6d933307613a93e39dce77ebb36dfc340c65e8
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-16 01:03:07 +0500

    contact page style and route

--- src/router.js
+++ src/router.js
@@ -1,41 +1,47 @@
-import ServicePage from './Pages/ServicePage.vue'
-import LandingPage from './Pages/LandingPage.vue'
-import AboutPage from './Pages/AboutPage.vue'
-import CareerPage from './Pages/CareerPage.vue'
-import ExtraInfo from './Pages/ExtraInfo.vue'
-import {createRouter,createWebHistory} from 'vue-router';
+import ServicePage from "./Pages/ServicePage.vue";
+import LandingPage from "./Pages/LandingPage.vue";
+import AboutPage from "./Pages/AboutPage.vue";
+import CareerPage from "./Pages/CareerPage.vue";
+import ExtraInfo from "./Pages/ExtraInfo.vue";
+import ContactPage from "./Pages/ContactPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    {
-        name:"LandingPage",
-        component:LandingPage,
-        path:"/"
-    },
-    {
-        name:"ServicePage",
-        component:ServicePage,
-        path:"/services"
-    },
-    {
-        name:"AboutPage",
-        component:AboutPage,
-        path:"/about"
-    },
-    {
-        name:"CareerPage",
-        component:CareerPage,
-        path:"/career"
-    },
-    {
-        name:"ExtraInfo",
-        component:ExtraInfo,
-        path:"/extra"
-    },
-]
+  {
+    name: "LandingPage",
+    component: LandingPage,
+    path: "/",
+  },
+  {
+    name: "ServicePage",
+    component: ServicePage,
+    path: "/services",
+  },
+  {
+    name: "AboutPage",
+    component: AboutPage,
+    path: "/about",
+  },
+  {
+    name: "CareerPage",
+    component: CareerPage,
+    path: "/career",
+  },
+  {
+    name: "ExtraInfo",
+    component: ExtraInfo,
+    path: "/extra",
+  },
+  {
+    name: "ContactPage",
+    component: ContactPage,
+    path: "/contact",
+  },
+];
 
 const router = createRouter({
-    history:createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
