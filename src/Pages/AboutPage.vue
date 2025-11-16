879c4d8e8487cdcf2f734284231afc2ce2bc2e51
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-16 13:12:53 +0500

    made about-hero-section

--- src/Pages/AboutPage.vue
+++ src/Pages/AboutPage.vue
@@ -1,15 +1,35 @@
 <script>
-    import NavbarComponent from '@/components/NavbarComponent.vue'; 
+    import NavbarComponent from '@/components/NavbarComponent.vue';
+    import HeroSection from '@/components/HeroSection.vue'; 
+    import FormComponent from '@/components/FormComponent.vue';
+    import FooterComponent from '@/components/FooterComponent.vue';
 
     export default{
         name:'AboutPage',
         components:{
             NavbarComponent,
+            FooterComponent,
+            HeroSection,
+            FormComponent,
         }
     }
 </script>
 
 <template>
-    <h1>About Page</h1>
+    
     <NavbarComponent/>
+    <HeroSection
+        heroClass="about-hero-content"
+        smallText="ABOUT US"
+        largeText="We create a better tomorrow<br>through AI and technology"
+        videoSrc="women.mp4"
+        ctaText="Get in Touch"
+        ctaLink="#form-section-id"
+    />
+
+    <FormComponent
+        mainText="Get to know us"
+    />
+    <FooterComponent/>
+
 </template>
