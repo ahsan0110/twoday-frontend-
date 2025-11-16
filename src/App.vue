c296352f85fdd26ffb7ed9be7a869c70750834eb
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-16 17:52:20 +0500

    added Loader

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,38 @@
 <template>
-  <router-view /> 
+  <div>
+    <div v-if="loadingState.loading" class="loading-overlay">
+      <div class="loader"></div>
+    </div>
+
+    <router-view />
+  </div>
 </template>
 
+<script setup>
+import { loadingState } from "./loading";
+</script>
+
+<style>
+.loading-overlay {
+  position: fixed;
+  inset: 0;
+  background: rgba(3, 3, 3, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+}
+
+.loader {
+  width: 60px;
+  height: 60px;
+  border: 5px solid #838383;
+  border-top-color: #e90000;
+  border-radius: 50%;
+  animation: spin 0.7s linear infinite;
+}
+
+@keyframes spin {
+  to { transform: rotate(360deg); }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -9,3 +9,17 @@ const app = createApp(App);
 
 app.use(router);
 app.mount('#app');
+
+
+import { loadingState } from "./loading";
+
+router.beforeEach((to, from, next) => {
+  loadingState.loading = true;
+  next();
+});
+
+router.afterEach(() => {
+  setTimeout(() => {
+    loadingState.loading = false;
+  }, 700);
+});
