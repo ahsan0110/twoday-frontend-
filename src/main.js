ebb41f736ecb13e9792e4cbc6162fe6097284e46
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-11-16 18:39:04 +0500

    responsiveness for mobile and tablet Hero Section

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '/src/style.css'
 import router from './router'
+import { loadingState } from "./loading";
 
 const app = createApp(App);
 
 app.use(router);
 app.mount('#app');
 
-
-import { loadingState } from "./loading";
 
 router.beforeEach((to, from, next) => {
   loadingState.loading = true;
